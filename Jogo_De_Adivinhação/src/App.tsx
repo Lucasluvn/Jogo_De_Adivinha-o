114f3c98139e46a075e39816ccfaa65c400373c3
Lucas <lucasluvn@gmail.com>
2026-07-06 22:09:50 -0300

    Criando a função de sortear as palavras

--- Jogo_De_Adivinhação/src/App.tsx
+++ Jogo_De_Adivinhação/src/App.tsx
@@ -1,4 +1,9 @@
 import styles from "./App.module.css"
+import { useEffect,useState } from "react"
+import {WORDS} from "./assets/utils/words"
+import type {Challenge} from "./assets/utils/words"
+
+
 import {Button} from "./components/Button"
 import { Input } from "./components/Input/Index"
 import {Letters} from "./components/Letters"
@@ -7,15 +12,37 @@ import {Header} from "./components/Header"
 import { LettersUsed } from "./components/LettersUsed"
 
 
+
 export default function App() {
+  console.log("App renderizou")
+const [challenge,setChallenge] = useState<Challenge| null>(null)
+const [attempts, setAttempts] = useState(0)
+const [letter, setLetter] = useState("")
 
   function handleRestartGame(){
     alert("REINICIAR O JOGO")
   }
+
+  function startGame(){
+ 
+  const index = Math.floor(Math.random()* WORDS.length)
+  const randomWord = WORDS[index]
+  
+  setAttempts(0)
+  setChallenge(randomWord)
+  setLetter("")
+
+  
+  
+}
+  useEffect(()=>{
+    startGame()
+  },[])
+
   return (
     <div className={styles.container}>
       <main>
-      <Header current={5} max={10} onRestart={handleRestartGame}/>
+      <Header current={attempts} max={10} onRestart={handleRestartGame}/>
       
       <Tip tip = "Linguagem de Programação"/>
     
@@ -43,5 +70,6 @@ export default function App() {
    </div>
     
   )
-
+ 
 }
+
